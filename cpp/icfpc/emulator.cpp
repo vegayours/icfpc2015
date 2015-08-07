3536b8c016ebb378401bc19d9858f83ed9b09818
Alexandr Likhanov <vegayours@gmail.com>
2015-08-08 02:50:04 +0500

    add simple seeded emulator with unit stream

--- cpp/icfpc/emulator.cpp
+++ cpp/icfpc/emulator.cpp
@@ -1,4 +1,5 @@
 #include "common/unit.h"
+#include "common/rnd_gen.h"
 
 #include "emulator/load_board.h"
 #include "emulator/show_board.h"
@@ -12,6 +13,96 @@
 
 using namespace folly;
 
+class TUnitStream {
+public:
+    TUnitStream(uint32_t seed, uint32_t limit, fbvector<NCommon::TUnit> units)
+        : Rnd(seed)
+        , Available(limit)
+        , Units(std::move(units))
+    {
+    }
+    bool Next(NCommon::TUnit& unit) {
+        if (Available > 0) {
+            size_t next = Rnd.Next() % Units.size();
+            unit = Units[next];
+            Available--;
+            return true;
+        }
+        return false;
+    }
+    void Reset(uint32_t seed, uint32_t limit) {
+        Available = limit;
+        Rnd.Reset(seed);
+    }
+private:
+    NCommon::TRndGen Rnd;
+    uint32_t Available;
+    fbvector<NCommon::TUnit> Units;
+};
+
+void RunBoard(NCommon::TBoard board, TUnitStream& stream) {
+    auto state = NEmulator::CreateState(board);
+    NCommon::TUnit unit, next;
+    stream.Next(unit);
+    unit = unit.PlaceToBoard(board);
+    NEmulator::ShowBoard(state, board, &unit);
+    int c;
+    fbstring commands;
+    bool hasCommand = false;
+    while ((c = getchar()) != '/') {
+        hasCommand = false;
+        switch (c) {
+            case '\n':
+            case EOF:
+                continue;
+            case 'p':
+                hasCommand = true;
+                next = unit.MoveLeft();
+                commands.push_back(c);
+                break;
+            case 'b':
+                hasCommand = true;
+                next = unit.MoveRight();
+                commands.push_back(c);
+                break;
+            case 'a':
+                hasCommand = true;
+                next = unit.MoveDownLeft();
+                commands.push_back(c);
+                break;
+            case 'l':
+                hasCommand = true;
+                next = unit.MoveDownRight();
+                commands.push_back(c);
+                break;
+            default:
+                fprintf(stderr, "Unknown move!\n");
+                break;
+        }
+        if (!hasCommand)
+            continue;
+        if (!board.IsValidUnit(next)) {
+            fprintf(stderr, "Invalid move, will lock now, unit commands: '%s'!\n", commands.c_str());
+            board.LockUnit(unit);
+            commands.clear();
+            if (!stream.Next(unit)) {
+                fprintf(stderr, "Unit stream end, final board:\n");
+                NEmulator::ShowBoard(state, board, nullptr);
+                return;
+            }
+            unit = unit.PlaceToBoard(board);
+            if (!board.IsValidUnit(unit)) {
+                fprintf(stderr, "Unable to place next unit, game will end!\n");
+                return;
+            }
+        }
+        else {
+            unit = std::move(next);
+        }
+        NEmulator::ShowBoard(state, board, &unit);
+    }
+}
+
 int main(int argc, const char** argv) {
     if (argc < 2) {
         fprintf(stderr, "Need more arguments: ./emulator problem.json\n");
@@ -23,31 +114,9 @@ int main(int argc, const char** argv) {
     printf("%s\n", toPrettyJson(problem).c_str());
     auto board = NEmulator::ParseBoard(problem);
     auto units = NEmulator::LoadUnits(problem);
-    auto unit = units[0].PlaceToBoard(board);
-    auto state = NEmulator::CreateState(board);
-    NEmulator::ShowBoard(state, board, unit);
-    int c;
-    while ((c = getchar()) != '/') {
-        switch (c) {
-            case '\n':
-            case EOF:
-                continue;
-            case 'p':
-                unit = unit.MoveLeft();
-                break;
-            case 'b':
-                unit = unit.MoveRight();
-                break;
-            case 'a':
-                unit = unit.MoveDownLeft();
-                break;
-            case 'l':
-                unit = unit.MoveDownRight();
-                break;
-            default:
-                fprintf(stderr, "Wrong move!\n");
-                break;
-        }
-        NEmulator::ShowBoard(state, board, unit);
-    }
+    uint32_t limit = problem["sourceLength"].asInt();
+    uint32_t seed = problem["sourceSeeds"][0].asInt();
+    TUnitStream stream(seed, limit, std::move(units));
+    RunBoard(board, stream);
+    return 0;
 }
