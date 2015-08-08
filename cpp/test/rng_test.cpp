70e446e2241b61a6f864d21563f6a6fb17abf280
Alexandr Likhanov <vegayours@gmail.com>
2015-08-08 14:02:11 +0500

    fix test

--- cpp/test/rng_test.cpp
+++ cpp/test/rng_test.cpp
@@ -2,7 +2,6 @@
 #include <gtest/gtest.h>
 
 TEST(TRndGenTest, CheckRng) {
-    using namespace NCommon;
     TRndGen gen(17);
     uint32_t expected[] = {0, 24107, 16552, 12125, 9427, 13152, 21440, 3383, 6873, 16117};
     size_t cnt = sizeof(expected) / sizeof(expected[0]);
